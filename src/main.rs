694a68664ab0b121196de38130f565356687836b
Dustin Cannon <dustin@shunyata.space>
2021-08-15 14:21:53 -0400

    simple

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
-use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{Write};
 use std::process::{Command, Stdio};
 
 fn main() {
 
     let v: Vec<String> = (0..100).map(|x| x.to_string()).collect();
 
-    let mut child = Command::new("bash").arg("-c")
-        .arg("parallel -n1 -a - 'echo -n \"$$ {#} {%} {} -- \";\
-              sleeptime=$(echo -n $(($RANDOM % 10)));\
-              echo sleep $sleeptime;\
-              sleep $sleeptime'")
-        //.arg("echo $$ {#} {%} {}")
+    //let mut child = Command::new("bash").arg("-c")
+    //    .arg("parallel -n1 -a - 'echo -n \"$$ {#} {%} {} -- \";\
+    //          sleeptime=$(echo -n $(($RANDOM % 10)));\
+    //          echo sleep $sleeptime;\
+    //          sleep $sleeptime'")
+    //    .stdin(Stdio::piped())
+    //    .stdout(Stdio::piped())
+    //    .spawn()
+    //    .expect("failed to execute");
+
+    let mut child = Command::new("parallel")
+        .arg("echo {%}-{#}-{}")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
         .expect("failed to execute");
 
     let mut stdin = child.stdin.take().expect("Failed to open stdin");
-    //let string_list = vec!["Foo".to_string(),"Bar".to_string()];
     let joined = v.join("\n");
-    //for s in v {
-    //    println!("{}", s)
-    //}
     std::thread::spawn(move || {
         stdin.write_all(joined.as_bytes()).expect("Failed to write to stdin");
     });
