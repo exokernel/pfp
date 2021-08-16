15ef58bf565fc6f0b6d6cdde80782005a2bfdb06
Dustin Cannon <dustin@shunyata.space>
2021-08-16 17:47:28 -0400

    work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,56 @@
-use std::io::{Write};
-use std::process::{Command, Stdio};
+use std::io::{Write,Result};
+use std::process::{Command, Stdio, Output};
+//use std::error::Error;
 
-fn main() {
-
-    let v: Vec<String> = (0..100).map(|x| x.to_string()).collect();
-
-    //let mut child = Command::new("bash").arg("-c")
-    //    .arg("parallel -n1 -a - 'echo -n \"$$ {#} {%} {} -- \";\
-    //          sleeptime=$(echo -n $(($RANDOM % 10)));\
-    //          echo sleep $sleeptime;\
-    //          sleep $sleeptime'")
-    //    .stdin(Stdio::piped())
-    //    .stdout(Stdio::piped())
-    //    .spawn()
-    //    .expect("failed to execute");
-
+//fn parallelize(command: &str, input: Vec<String>) -> Result<Output> {
+/// Execute command in a subprocess using Gnu Parallel with given input
+/// Runs parallel instances of command with on item of input per instance
+/// E.g. input ['a','b','c'] -> parallel-exec [command 'a', command 'b', command 'c']
+fn parallelize(command: &str, input: &'static [String]) -> Result<Output> {
+    
     let mut child = Command::new("parallel")
-        .arg("echo {%}-{#}-{}")
+        .arg(command)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
         .expect("failed to execute");
 
     let mut stdin = child.stdin.take().expect("Failed to open stdin");
-    let joined = v.join("\n");
     std::thread::spawn(move || {
-        stdin.write_all(joined.as_bytes()).expect("Failed to write to stdin");
+        stdin.write_all(input.join("\n").as_bytes()).expect("Failed to write to stdin");
     });
     
-    let output = child.wait_with_output().expect("Failed to read stdout");
-    println!("{}", String::from_utf8_lossy(&output.stdout));
+    child.wait_with_output()//.expect("Failed to read stdout");
+}
+
+fn main() {
+
+    
+    loop {
+        // 1. get a whole set of input (e.g. names of all files in some directory)
+        //    if there's no input sleep for a while and try again
+        let v: Vec<String> = (0..101).map(|x| x.to_string()).collect();
+        let v2: &[&str]    = vec!["Hello", "world!"];
+        let v3: &[String]  = vec!["Hello".to_owned(), "world!".to_owned()];
+
+        // 2. break it into chucks of a specified size
+        let chunksize = 50;
+        let numchunks = v.len()/chunksize; // 2
+        let leftover = v.len() % chunksize; // 1
+        // ??? how to break input up into chunks
+  
+        // Each chunk is a reference to slice of our Vec<String>:  &[String] &["foo", "bar",...,"baz"]
+        // chunk 1: &v[chunksize*0..chunksize] [0..50] 0-49 50-things
+        // chunk 2: &v[chunksize*1..chunksize] [50..100] 50-99 50-things
+        // chunk 3: &v[chunksize*2..(chunksize*2+1)] [100..101] 100 1-thing
+
+        // 3. process each chunk in parallel 
+        for c in chunks {
+            // c is a chunk w/ chunksize elements
+            let output = parallelize("echo {%}-{#}-{}", c);
+            println!("{}", String::from_utf8_lossy(&output.unwrap().stdout));
+        }
+
+        // 4. Do any necessary postprocessing
+    }
 }
