8deb8f736039bb14c1b56844b645a4517d0b4cec
Dustin <dustin@shunyata.space>
2021-10-22 09:29:17 -0400

    real aws-upload.sh

--- src/aws-upload.sh
+++ src/aws-upload.sh
@@ -1,19 +1,77 @@
 #!/usr/bin/env bash
 
+S3BUCKET="s3doj"
+S3FOLDER="upload-test"
+DBTABLE="files"
+
 write_to_db() {
-   echo writing $file to db 
+    fullpath=$1
+    filename=$(basename $fullpath)
+    directory=$(dirname $fullpath)
+
+    dbinsert="INSERT INTO $DBTABLE (host, filename, fullpath, directory, upload_finished) VALUES\
+             (\"$(hostname -f)\", \"$filename\", \"$fullpath\", \"$directory\", NOW())"
+
+    echo writing info for $f to the database
+    # dbhost, database, user, pass in ~/.my.cnf under [client_test]
+    mysql --defaults-group-suffix=_test -e "$dbinsert" || (echo failed to write to database; exit 1)
 }
 
 cleanup() {
-    echo cleaning up $file and $file.md5
-    rm $1 $2
+    f=$1
+    echo would remove $f
+    # Don't actually remove it until we're ready for prime time
+    #rm $1
 }
 
-file=$1
+path=$1
+file=$(basename $path)
+directory=$(dirname $path)
 
+# WARNING: Big assumption here! We are passed a path that looks like /data/aws/YYYY-mm-dd/filename
+prefix="/data/aws/"
+datedir=${directory#$prefix} # get rid of prefix, leaving only the date portion
+year=${datedir%-*-*}
+month=$(echo $datedir | sed -r 's/[0-9]{4}-([0-9]{2})-[0-9]{2}/\1/')
+
+#echo path $1
+#echo file $file
+#echo directory $directory
+#echo datedir $datedir
+#echo year $year
+#echo month $month
+#exit
+
+# work out of the same directory as the file
+cd $directory
+
+# Log start
+starttime="$(date "+%Y-%m-%d %T")"
+echo "$starttime: START FILE: $file"
+
+# create a file with our md5 checksum
 md5sum $file > $file.md5
 
-#(tar -cvz -f - $file $file.md5 | ./encrypt.sh | ./upload.sh) && write_to_db && cleanup $file $file.md5
+# tarball-encrypt-upload pipeline
+# if we successfully upload a file then we write a record to the database
+# and then remove the original file
+(tar -cvz -f - $file.md5 $file |\
+gpg --batch --no-tty --encrypt --cipher-algo AES256 --compress-algo none -r 1539150C -o - |\
+#aws s3 cp --profile deeparchive --storage-class DEEP_ARCHIVE - s3://$S3BUCKET/$S3FOLDER/$file.tgz.crypt)\
+aws s3 cp --profile deeparchive - s3://$S3BUCKET/$S3FOLDER/$file.tgz.crypt)\
+&& write_to_db $path\
+&& cleanup $file
+
+# cleanup the md5 regardless
+test -f $file.md5 && rm $file.md5
+
+# Log end
+endtime="$(date "+%Y-%m-%d %T")"
+echo "$endtime: END FILE: $file"
+
+#------------------------------------
+# Some Notes
+#------------------------------------
 
 #tar -cvz -f - $file $file.md5
 
@@ -29,3 +87,15 @@ md5sum $file > $file.md5
 #else:
 #aws_string = '/usr/bin/mtglacier --config /root/.aws/glacier.cfg --journal "/var/log/glacier/'+ymd+'.log" --vault "' \
 #+VAULT+'" --dir '+LOCAL_DIR+' --filename ' + encrypted_file_path + ' --concurrency 30 upload-file'
+
+#CREATE TABLE `files` (
+#  `id` int(11) NOT NULL AUTO_INCREMENT,
+#  `host` varchar(50) DEFAULT NULL,
+#  `filename` varchar(100) NOT NULL,
+#  `fullpath` varchar(250) NOT NULL,
+#  `directory` varchar(250) NOT NULL,
+#  `upload_finished` datetime DEFAULT NULL,
+#  PRIMARY KEY (`id`),
+#  UNIQUE KEY `filename` (`filename`),
+#  KEY `host` (`host`)
+#) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4
