8e02c64d872452d46b69fab2749f58819f880a2f
Dustin <dustin@shunyata.space>
2021-10-22 09:54:09 -0400

    don't need these

--- src/encrypt.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#echo encrypting file
-
-while read line
-do
-  echo encrypting "$line"
-done #< "/dev/stdin"
--- src/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#echo uploading to aws
-
-while read line
-do
-  echo uploading "$line"
-done #< "/dev/stdin"
