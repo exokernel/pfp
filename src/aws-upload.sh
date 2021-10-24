2ee246e1751d227d4068266ff619b7d215697cd9
Dustin Cannon <dustin@shunyata.space>
2021-10-24 12:09:21 -0400

    shellcheck fixes

--- src/aws-upload.sh
+++ src/aws-upload.sh
@@ -17,29 +17,29 @@ tsecho() {
 
 write_to_db() {
     fullpath=$1
-    filename=$(basename $fullpath)
-    directory=$(dirname $fullpath)
+    filename=$(basename "$fullpath")
+    directory=$(dirname "$fullpath")
 
     dbinsert="INSERT INTO $DBTABLE (host, filename, fullpath, directory, upload_finished) VALUES\
              (\"$(hostname -f)\", \"$filename\", \"$fullpath\", \"$directory\", NOW())"
 
-    echo writing info for $filename to the database
+    tsecho "writing info for $filename to the database"
     # dbhost, database, user, pass in ~/.my.cnf under [client_test]
     mysql --defaults-group-suffix=_test -e "$dbinsert" || (echo failed to write to database && return 1)
 }
 
 cleanup() {
     f=$1
-    echo cleaningup lockfile for $f
-    rm $f.lock
-    echo would remove $f
+    tsecho "cleaning up lockfile for $f"
+    rm "$f".lock
+    tsecho "would remove $f"
     # Don't actually remove it until we're ready for prime time
     #rm $1
 }
 
 path=$1
-file=$(basename $path)
-directory=$(dirname $path)
+file=$(basename "$path")
+directory=$(dirname "$path")
 
 # WARNING: Big assumption here! We are passed a path that looks like /data/aws/YYYY-mm-dd/filename
 prefix="/data/aws/"
@@ -60,34 +60,34 @@ month=$(echo $datedir | sed -r 's/[0-9]{4}-([0-9]{2})-[0-9]{2}/\1/')
 #exit
 
 # work out of the same directory as the file
-cd $directory
+cd "$directory" || bail "failed to chdir to $directory"
 
 # Create a lockfile before starting the upload pipeline.
 # If the lockfile for this file already exists then we bail.
 # The lockfile is only cleaned up after a successful upload and db insert.
 # This keeps us from retrying any failed uploads
-test -f $file.lock && bail "lockfile for $file already exists"
-touch $file.lock
+test -f "$file".lock && bail "lockfile for $file already exists"
+touch "$file".lock
 
 # Log start
 #starttime="$(date "+%Y-%m-%d %T")"
 tsecho "START FILE: $file"
 
 # create a file with our md5 checksum
-md5sum $file > $file.md5
+md5sum "$file" > "$file".md5
 
 # tarball-encrypt-upload pipeline
 # if we successfully upload a file then we write a record to the database
 # and then remove the original file
-(tar -cvz -f - $file.md5 $file |\
+(tar -cvz -f - "$file".md5 "$file" |\
 gpg --batch --no-tty --encrypt --cipher-algo AES256 --compress-algo none -r 1539150C -o - |\
 #aws s3 cp --profile deeparchive --storage-class DEEP_ARCHIVE - s3://$S3BUCKET/$S3FOLDER/$file.tgz.crypt)\
-aws s3 cp --profile deeparchive - s3://$S3BUCKET/$S3FOLDER/$file.tgz.crypt)\
-&& write_to_db $path\
-&& cleanup $file
+aws s3 cp --profile deeparchive - s3://$S3BUCKET/$S3FOLDER/"$file".tgz.crypt)\
+&& write_to_db "$path"\
+&& cleanup "$file"
 
 # cleanup the md5 regardless
-test -f $file.md5 && (echo cleaning up $file.md5; rm $file.md5)
+test -f "$file".md5 && (echo cleaning up "$file".md5; rm "$file".md5)
 
 # Log end
 #endtime="$(date "+%Y-%m-%d %T")"
