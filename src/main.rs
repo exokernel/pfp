585e774c4d4986b8424368b61b7af518e60f296d
Dustin Cannon <dustin@shunyata.space>
2021-10-26 15:19:09 -0400

    Fix leftover bug

--- src/main.rs
+++ src/main.rs
@@ -148,13 +148,21 @@ fn get_files(dir: &Path, extensions: &Vec<&str>, files: &mut Vec<String>) -> io:
 /// Runs chunk_size instances of command via Gnu Parallel each with a single file from the chunk as argument
 /// The number of parallel jobs used for the chunk is determined by the slots argument, which specifies the
 /// number of job slots for Gnu Parallel to use
-fn process_chunk(chunk_num: usize, chunk_size: usize, slots: &str, command: &String, files: &Vec<String>) 
+fn process_chunk(chunk_num: usize, chunk_size: usize, leftover: usize, slots: &str, command: &String, files: &Vec<String>) 
    -> Result<(), Box<dyn Error>>{
     debug!("chunk {} ({}): START", chunk_num + 1, chunk_size);
     let index = chunk_size*chunk_num;
-    let chunk = (&files[index..(index+chunk_size)]).to_vec();
+    let chunk;
+    if leftover == 0 {
+        chunk = (&files[index..(index+chunk_size)]).to_vec();
+        debug!("chunk num {} size {}", chunk_num+1, chunk_size);
+        debug!("chunk start: {} chunk_end: {}", index, index+chunk_size-1);
+    } else {
+        chunk = (&files[index..(index+leftover)]).to_vec(); 
+        debug!("chunk num {} size {}", chunk_num+1, leftover);
+        debug!("chunk start: {} chunk_end: {}", index, index+leftover-1);
+    }
     //let output = parallelize(&command, slots, chunk);
-    debug!("chunk num {} size {}", chunk_num, chunk_size);
     for f in chunk.iter() {
         debug!("f: {}", f);
     }
@@ -213,11 +221,11 @@ fn run(chunk_size: usize,
         // chunk 3:  [100..101] 100 1-thing
 
         for n in 0..num_chunks {
-            process_chunk(n, chunk_size, slots, &command, &files)?;
+            process_chunk(n, chunk_size, 0, slots, &command, &files)?;
         }
         // last chunk
         if leftover != 0 {
-            process_chunk(num_chunks, leftover, slots, &command, &files)?;
+            process_chunk(num_chunks, chunk_size, leftover, slots, &command, &files)?;
         }
 
         // 3. Do any necessary postprocessing
